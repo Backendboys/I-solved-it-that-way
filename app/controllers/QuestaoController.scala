280a82c40ddfb9cda041f00435464977d9ce20dd
Jonathan Batista de Oliveira Silva <jonathan.b.d.o.s@gmail.com>
2016-08-21 19:02:12 -0300

    Added edit to questions controller

--- app/controllers/QuestaoController.scala
+++ app/controllers/QuestaoController.scala
@@ -37,9 +37,6 @@ class QuestaoController @Inject()(db: Database) extends Controller {
         val questao: Option[Questao] = dao.insert(
           userData.enunciado, userData.descricao)
 
-        println("Questão adicionada: ")
-        println(questao)
-
         var index = 1
 
         questao match {
@@ -49,15 +46,13 @@ class QuestaoController @Inject()(db: Database) extends Controller {
               request.body.asMultipartFormData.map(_.files)
             val arquivosTesteIO: Option[Seq[File]] =
               arquivosTeste.map { fileSeq =>
-                fileSeq map { file =>
+                fileSeq.filterNot(_.filename == "") map { file =>
                   val dir = new File("./app/testfiles/" +
                     f"${value.codigo}%02d")
                   dir.mkdirs()
                   val newFile = new File(dir.getPath() + "/" + f"$index%02d")
                   index += 1
                   val teste: Option[Teste] = testeDao.insert(newFile, "U", value)
-                  println("Teste adicionado:")
-                  println(teste)
                   file.ref.moveTo(newFile)
               }
             }
@@ -76,7 +71,53 @@ class QuestaoController @Inject()(db: Database) extends Controller {
   }
 
   def edit(codigo: Long) = Action { implicit request =>
-    Ok(views.html.questao.edit())
+    val questao: Questao = new QuestaoDAO(db).get(codigo)
+    val testes: List[Teste] = new TesteDAO(db).list(Some(codigo))
+    Ok(views.html.questao.edit(QuestaoForm.form, questao, testes))
+  }
+
+  def update(codigo: Long) = Action { implicit request =>
+    val dao = new QuestaoDAO(db)
+    val testeDao = new TesteDAO(db)
+    val instance: Questao = dao.get(codigo)
+    val testes: List[Teste] = testeDao.list(Some(codigo))
+    QuestaoForm.form.bindFromRequest.fold(
+      formWithErrors => {
+        BadRequest(views.html.questao.edit(formWithErrors,
+          instance, testes))
+      },
+      userData => {
+        instance.enunciado = userData.enunciado
+        instance.descricao = userData.descricao
+        val questao: Option[Questao] = dao.update(instance)
+
+        var index = testeDao.count(Some(instance.codigo)) + 1
+
+        questao match {
+          case Some(value) => {
+            // SALVANDO ARQUIVOS DE TESTE
+            val arquivosTeste: Option[Seq[FilePart[TemporaryFile]]] =
+              request.body.asMultipartFormData.map(_.files)
+            val arquivosTesteIO: Option[Seq[File]] =
+              arquivosTeste.map { fileSeq =>
+                fileSeq.filterNot(_.filename == "") map { file =>
+                  val dir = new File("./app/testfiles/" +
+                    f"${value.codigo}%02d")
+                  dir.mkdirs()
+                  val newFile = new File(dir.getPath() + "/" + f"$index%02d")
+                  index += 1
+                  val teste: Option[Teste] = testeDao.insert(newFile, "U", value)
+                  file.ref.moveTo(newFile)
+              }
+            }
+          }
+          case None => println("Erro ao atualizar questão")
+        }
+
+        Redirect(routes.ProfessorController.home)
+
+      }
+    )
   }
 
 }
